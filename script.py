4adbc1fc8ade3312d53e06d83460a3210b306111
Thomas Canava <canava.thomas@etu.unice.fr>
2018-03-01 22:05:13 +0100

    Merge remote-tracking branch 'origin/imrov_naive'

--- script.py
+++ script.py
@@ -1,15 +1,54 @@
 import math
 import sys
 
+sys.stdin = open("inputs/b_should_be_easy.in", 'r')
 
-# sys.stdin = open("inputs/a_example.in", 'r')
 
 def dist(a, b, x, y):
-    return math.fabs(a - x) + math.fabs(b - y)
+    return abs(a - x) + abs(b - y)
 
 
 def dist_ride(r):
     return dist(r['a'], r['b'], r['x'], r['y'])
+
+
+def finish(r):
+    return r['f']
+
+
+def start(r):
+    return r['s']
+
+
+def p(j):
+    rj = rides[j]
+    m = j - 1
+    for k in reversed(rides[:j]):
+        if m not in toIgnore:
+            if (k['f'] + dist(k['x'], k['y'], rj['a'], rj['b'])) < rj['f'] - dist_ride(rj):
+                break
+        m -= 1
+    if m == 0:
+        P[j] = 0
+        return
+
+    P[j] = m
+
+
+def bondOpt():
+    M[0] = -1
+    for j in range(1, len(rides)):
+        M[j] = max(dist_ride(rides[j]) + M[P[j]], M[j - 1])
+
+
+def find_solution(j, tab):
+    if j == -1:
+        return tab
+    elif dist_ride(rides[j]) + M[P[j]] > M[j - 1]:
+        tab.append(j)
+        return find_solution(P[j], tab)
+    else:
+        return find_solution(j - 1, tab)
 
 
 (R, C, F, N, B, T) = tuple(map(int, input().split()))
@@ -22,13 +61,21 @@ for l in [input() for _ in range(N)]:
     i += 1
 
 vehicles = []
+M = [0] * len(rides)
+P = [0] * len(rides)
+# Put code here
 
+toIgnore = []
 rides.sort(key=lambda r: r['f'])
+for i in range(len(rides)):
+    p(i)
 
-i = 0
-for i in range(F):
-    r = rides[i]
-    vehicles.append("1 " + str(r['i']))
+for j in reversed(range(len(rides))):
+    if j not in toIgnore:
+        bondOpt()
+        tabLol = find_solution(j, [])
+        vehicles.append(str(len(tabLol)) + " " + " ".join(list(map(str, tabLol))))
+        toIgnore.extend(tabLol)
 
 for v in vehicles:
     print(v)
