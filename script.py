e9550ed4f178904bb6a6645f405c30933d14b1dd
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-03-01 18:59:39 +0100

    input parsing + examples

--- script.py
+++ script.py
@@ -1,3 +1,7 @@
-N = int(input())
-lines = [input().split() for _ in range(N)]
+(R, C, F, N, B, T) = tuple(map(int, input().split()))
+rides = []
+
+for l in [input() for _ in range(N)]:
+    a = l.split()
+    rides.append({'a': a[0], 'b': a[1], 'x': a[2], 'y': a[3], 's': a[4], 'f': a[5]})
 
