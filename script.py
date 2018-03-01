b7fad1c9b3c979d6e71336b22a466f188dd557f1
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-03-01 22:30:20 +0100

    best

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import math
 import sys
 
-#sys.stdin = open("inputs/e_high_bonus.in", 'r')
+#sys.stdin = open("inputs/d_metropolis.in", 'r')
 
 def dist(a, b, x, y):
     return math.fabs(a-x) + math.fabs(b-y)
@@ -30,30 +30,45 @@ vehicles = [{"rides": [], "lastest": 0, "pos": [0, 0]} for _ in range(F)]
 
 rides.sort(key=lambda r: start(r) + dist(0,0, r['a'], r['b']))
 
-find = False
+for v in vehicles:
+    r = rides.pop(0)
+    v["rides"].append(r['i'])
+    v["lastest"] = v["lastest"] + dist_ride(r)
+    v["pos"] = [r['x'], r['y']]
 
-for r in rides:
-    possible = []
-    if B == 1000:
-        possible = [v for v in vehicles if v["lastest"] + dist(r["a"], r["b"], v["pos"][0], v["pos"][1]) <= r["s"]]
-        if not possible:
-            possible = [v for v in vehicles if
-                        v["lastest"] + dist(r["a"], r["b"], v["pos"][0], v["pos"][1]) + dist_ride(r) <= r["f"]]
+rides.sort(key=lambda r: start(r))
 
-    else:
-        possible = [v for v in vehicles if v["lastest"] + dist(r["a"], r["b"], v["pos"][0], v["pos"][1]) + dist_ride(r) <= r["f"]]
-
+while rides:
+    r = rides.pop(0)
+    possible = [v for v in vehicles if v["lastest"] + dist(v["pos"][0], v["pos"][1], r['a'], r['b']) + dist_ride(r) < r['f']]
     if not possible:
         continue
-    v = min(possible, key=lambda v: v["lastest"])
-    v["rides"].append(r["i"])
-    v["lastest"] = r["f"]
-    v["pos"] = [r["x"], r["y"]]
+    tmp = min(possible, key=lambda v: (v["lastest"] + dist(v["pos"][0], v["pos"][1], r['a'], r['b'])) - r['f'])
+    vehicles.remove(tmp)
+    v = min(possible, key=lambda v: (v["lastest"] + dist(v["pos"][0], v["pos"][1], r['a'], r['b'])) - r['f'])
+    vehicles.append(tmp)
 
-
-
+    v["rides"].append(r['i'])
+    v["lastest"] = v["lastest"] + dist_ride(r)
+    v["pos"] = [r['x'], r['y']]
 
 for v in vehicles: print(str(len(v["rides"])) + " " + " ".join(map(str, v["rides"])))
+
+"""lastest = r['f']
+pos = [r['x'], r['y']]
+v.append(r)
+
+while lastest < T:
+    r2 = min([r for rides in rides if dist(pos[0], pos[1], r['a'], r['b']) + lastest + dist_ride(r) < r['f']], key=lambda r: dist(pos[0], pos[1], r['a'], r['b']) + r['s'])
+    rides.remove(r2)
+    v.append(r2)
+    lastest = r2['f']
+    pos = [r2['x'], r2['y']]"""
+
+
+
+
+
 
 """if not rides:
     vehicles.append([])
